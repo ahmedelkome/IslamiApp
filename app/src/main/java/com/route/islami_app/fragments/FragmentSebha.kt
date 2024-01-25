50de2e893881e037599ff770c2556db01939615e
ahmed elkomy <ahmedelkomy@gmail.com>
2024-01-25 22:15:33 +0200

    + Finished Sebha Fragment

--- app/src/main/java/com/route/islami_app/fragments/FragmentSebha.kt
+++ app/src/main/java/com/route/islami_app/fragments/FragmentSebha.kt
@@ -6,20 +6,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.route.islami_app.R
+import com.route.islami_app.databinding.FragmentSebhaBinding
 
 
 class FragmentSebha : Fragment() {
-
-
+    lateinit var binding: FragmentSebhaBinding
+    var counter = 0
+    lateinit var azkarList : MutableList<String>
+    var currentZakr = 0
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sebha, container, false)
+
+         binding = FragmentSebhaBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        azkarList = resources.getStringArray(R.array.listofazkar).toMutableList()
+        binding.zakr.text = azkarList[currentZakr]
+        onSebhaClick()
+    }
+
+    private fun onSebhaClick() {
+        binding.bodySebha.setOnClickListener {
+            binding.bodySebha.rotation += (360/33).toFloat()
+            if (counter<33)
+            {
+                counter++
+            }
+            else
+            {
+                counter = 0
+                currentZakr =
+                    if (currentZakr<azkarList.size-1)
+                    {
+                        ++currentZakr
+                    }else
+                        0
+                binding.zakr.text = azkarList[currentZakr]
+            }
+            binding.counterDhikr.text = counter.toString()
+        }
+
+
     }
 }
